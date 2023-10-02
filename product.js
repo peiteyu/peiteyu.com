957964de8a21d5c3af36829e03f0943c3285ce91
peiteyu <143827760+peiteyu@users.noreply.github.com>
2023-10-02 10:36:00 +0800

    Add files via upload

--- product.js
+++ product.js
@@ -1,58 +1,94 @@
 const images = ["葵地淇DMpage (1).png", "葵地淇DMpage (2).png", "葵地淇DMpage (3).png","葵地淇DMpage (4).png","葵地淇DMpage (5).png"]; // 图片文件名数组
-let currentIndex = 0; // 当前显示的图片索引
+let currentIndex = 0; // 當前顯示的圖片索引
 const productImage = document.getElementById("productImage");
 const dotsContainer = document.getElementById("dotsContainer");
 const prevButton = document.getElementById("prevButton");
 const nextButton = document.getElementById("nextButton");
 
-// function updateImage() {
-//   const imageUrl = images[currentIndex];
-//   productImage.src = imageUrl;
-// }
-
 function updateImage() {
     const imageUrl = images[currentIndex];
-    productImage.style.transform = "scale(0)"; // 先缩小
+    productImage.style.transform = "scale(0)"; // 先縮小
     setTimeout(function () {
-      productImage.src = imageUrl; // 更新图片
+      productImage.src = imageUrl; // 更新圖片
       productImage.style.transform = "scale(1)"; // 再放大
-    }, 300); // 延迟300毫秒以便让缩小动画完成
+    }, 300); // 延遲300毫秒以便让缩小动画完成
 }
 
 function updateDots() {
-    dotsContainer.innerHTML = ""; // 清空之前的指示点
-    for (let i = 0; i < images.length; i++) {
-      const dot = document.createElement("div");
-      dot.classList.add("dot");
-      if (i === currentIndex) {
-        dot.classList.add("active-dot"); // 高亮显示当前图片的指示点
-      }
-      dot.addEventListener("click", function () {
-        currentIndex = i;
-        updateImage();
-      });
-      dotsContainer.appendChild(dot);
+  // 清空現有的指示点
+  dotsContainer.innerHTML = "";
+  // 重新生成dot元素
+  for (let i = 0; i < images.length; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    
+    // 設置當前圖片的指示點為被點選狀態
+    if (i === currentIndex) {
+      dot.classList.add("active-dot");
     }
+
+    dot.addEventListener("click", function () {
+      currentIndex = i;
+      updateImage();
+      updateDots(); // 更新指示點狀態
+    });
+    // 再添加至dotsContainer
+    dotsContainer.appendChild(dot);
+  }
 }
 
-prevButton.addEventListener("click", function () {
-  if (currentIndex > 0) {
-    currentIndex--;
-    updateImage();
-  }
-});
+// prevButton.addEventListener("click", function () {
+//   if (currentIndex > 0) {
+//     currentIndex--;
+//     updateImage();
+//   }
+// });
 
-nextButton.addEventListener("click", function () {
-  if (currentIndex < images.length - 1) {
-    currentIndex++;
-    updateImage();
-  }
-});
+// nextButton.addEventListener("click", function () {
+//   if (currentIndex < images.length - 1) {
+//     currentIndex++;
+//     updateImage();
+//   }
+// });
 
 
-// 初始化显示第一张图片
+// 初始化顯示第一張圖片
 updateImage();
+// 初始化顯示索引
+updateDots();
 
+
+// let isDragging = false;
+// let startPosX = 0;
+// let currentTranslate = 0;
+
+// productImage.addEventListener("mousedown", (e) => {
+//   isDragging = true;
+//   startPosX = e.clientX;
+//   productImage.style.cursor = "grabbing";
+// });
+
+// document.addEventListener("mousemove", (e) => {
+//   if (!isDragging) return;
+//   const dragDistance = e.clientX - startPosX;
+//   productImage.style.transform = `translateX(${currentTranslate + dragDistance}px)`;
+// });
+
+// document.addEventListener("mouseup", (e) => {
+//   if (!isDragging) return;
+//   isDragging = false;
+//   productImage.style.cursor = "grab";
+
+//   if (e.clientX - startPosX > 50 && currentIndex > 0) {
+//     currentIndex--;
+//   } else if (startPosX - e.clientX > 50 && currentIndex < images.length - 1) {
+//     currentIndex++;
+//   }
+
+//   currentTranslate = -currentIndex * productImage.clientWidth;
+//   productImage.style.transform = `translateX(${currentTranslate}px)`;
+//   updateDots();
+// });
 
 // 選單
 const hambur = document.getElementById('hambur_active');
