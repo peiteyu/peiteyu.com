bfe75cf45a5f44964199b69d6be267dd4f329d63
peiteyu <143827760+peiteyu@users.noreply.github.com>
2023-11-03 16:18:52 +0800

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,24 @@
+const listItems = document.querySelectorAll('li');
+let delay = 0;
+window.addEventListener('load', function() {   
+    listItems.forEach(item => {
+      setTimeout(() => {
+        item.style.opacity = '1';
+      }, delay);
+      delay += 200; // 延遲每個項目的顯示時間
+    });
+});
+
+// 選單
+const hambur = document.getElementById('hambur_active');
+const navbar = document.getElementById('navbar_active');
+hambur.addEventListener('click',function(){
+    if(!hambur.classList.contains('active')){
+        hambur.classList.add('active');
+        navbar.classList.add('active');
+    }
+    else{
+        hambur.classList.remove('active');
+        navbar.classList.remove('active');
+    }
+})
